2ff6d79c15f31f1308362f91440f5d4e0a8a6898
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 10:28:59 -0600

    Fix

--- src/main/java/dev/flexibyte/flexibyteapi/FlexiByteAPI.java
+++ src/main/java/dev/flexibyte/flexibyteapi/FlexiByteAPI.java
@@ -13,28 +13,30 @@ import java.util.logging.Logger;
 
 public final class FlexiByteAPI extends JavaPlugin {
 
-    private Plugin plug;
-    private Logger logger;
+    //private Plugin plug = null;
+    private Logger logger = null;
 
-    private ParticleLibrary particleLib;
-    private ItemTools itemTools;
-    private PlayerTools playerTools;
-    private StringTools stringTools;
-    private WorldTools worldTools;
+    private ParticleLibrary particleLib = null;
+    private ItemTools itemTools = null;
+    private PlayerTools playerTools = null;
+    private StringTools stringTools = null;
+    private WorldTools worldTools = null;
 
-    public FlexiByteAPI(Plugin plug){
+   /* public FlexiByteAPI(Plugin plug){
         //get plugin instance
         this.plug = plug;
     }
 
+    */
+
     @Override
     public void onEnable() {
         //get logger
-        this.logger = plug.getLogger();
+        this.logger = this.getLogger();
 
         logger.log(Level.INFO, "Successfully connected");
 
-        particleLib = new ParticleLibrary(plug);
+        particleLib = new ParticleLibrary(this);
         logger.log(Level.INFO, "Loaded particle library");
 
         itemTools = new ItemTools();
