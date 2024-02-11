57e350d7863f83ed5d48c2a813c2205942a4d350
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 17:08:01 -0600

    FlexiCommand

--- src/main/java/dev/flexibyte/flexibyteapi/FlexiByteAPI.java
+++ src/main/java/dev/flexibyte/flexibyteapi/FlexiByteAPI.java
@@ -5,8 +5,8 @@ import dev.flexibyte.flexibyteapi.tools.ItemTools;
 import dev.flexibyte.flexibyteapi.tools.PlayerTools;
 import dev.flexibyte.flexibyteapi.tools.StringTools;
 import dev.flexibyte.flexibyteapi.tools.WorldTools;
+import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,17 +24,20 @@ public final class FlexiByteAPI {
 
 
 
-    public void initialize(Plugin plug) {
+    public void initialize(Plugin plug, String dev, String client) {
         this.plug = plug;
 
         //get logger
         this.logger = plug.getLogger();
 
+        //log connected
         logger.log(Level.INFO, "Successfully connected");
 
+        //log libraries
         particleLib = new ParticleLibrary(plug);
         logger.log(Level.INFO, "Loaded particle library");
 
+        //log tools
         itemTools = new ItemTools();
         logger.log(Level.INFO, "Loaded item tools");
         playerTools = new PlayerTools();
@@ -44,6 +47,11 @@ public final class FlexiByteAPI {
         worldTools = new WorldTools(plug);
         logger.log(Level.INFO, "Loaded world tools");
 
+        //log commands
+        FlexiCommand command = new FlexiCommand("flexi" + plug.getName(), "Get FlexiByte info about " + plug.getName(), "USAGE: /flexi" + plug.getName(), null);
+        command.register(plug, "Woods", "none", stringTools);
+        ((CraftServer) plug.getServer()).getCommandMap().register("flexi" + plug.getName(), command);
+        logger.log(Level.INFO, "Registered /flexi" + plug.getName());
 
     }
 
---
+++ src/main/java/dev/flexibyte/flexibyteapi/FlexiCommand.java
@@ -0,0 +1,44 @@
+package dev.flexibyte.flexibyteapi;
+
+import dev.flexibyte.flexibyteapi.tools.StringTools;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
+
+public class FlexiCommand extends BukkitCommand {
+
+    private String dev = "";
+    private String client = "";
+    private Plugin plugin = null;
+    private StringTools stringTools = null;
+
+    protected FlexiCommand(String name, String description, String usageMessage, List<String> aliases) {
+        super(name, description, usageMessage, aliases);
+    }
+
+
+    public void register(Plugin plugin, String dev, String client, StringTools stringTools){
+        this.plugin = plugin;
+        this.dev = dev;
+        this.client = client;
+        this.stringTools = stringTools;
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        //get message
+        String message = "&3&lFlexiByte Services;&3Plugin: &b" + plugin.getName() + ";&3Version:" + plugin.getServer().getVersion() +";&3Developer: &b" + dev + ";&3Client &b" + client+ ";&e&oGet your plugin today! https://flexibyte.dev";
+        String[] send = message.split(";");
+
+        //send message
+        for(String s : send){
+            sender.sendMessage(stringTools.format(s));
+        }
+        return true;
+    }
+}
